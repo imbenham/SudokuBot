98e8d64e5b31fe960f423fa4a73ceaefcd6a7595
imbenham <isaacbenham@Isaacs-MacBook-Pro.local>
2015-04-17 16:22:39 -0400

    boxes laid out by class that makes use of generic methods

--- SudokuLogic.swift
+++ SudokuLogic.swift
@@ -8,30 +8,74 @@
 
 import UIKit
 
+extension SudokuBoard {
+    // add game-logic related methods
+    func getBoxAtIndex(index: Int) -> Box {
+        let boxOfBoxes = self.boxes as! [Box]
+        return boxOfBoxes[index]
+    }
+}
+
+extension Box {
+    // add game-logic related methods
+    func getTileAtIndex(index: Int) -> Tile {
+        return boxes[index] as! Tile
+    }
+}
+
+extension Tile {
+    // add game-logic related methods
+    typealias TileIndex = (Box:Int, Tile:Int)
+    
+    func tileIndex() -> TileIndex {
+        return (parentSquare!.index, index)
+    }
+    
+    func indexString() -> String {
+        let box = tileIndex().0
+        let tile = tileIndex().1
+        return "This tile's index is: \(box).\(tile) "
+    }
+
+}
+
 class SudokuController: UIViewController {
     // all the logic of the Sudoku game will be defined here, allowing the main view controller to focus on the task of handling user events
     
     typealias TileIndex = (Box:Int, Tile:Int)
-    let board:SudokuBoard = SudokuBoard(frame: CGRectZero)
+    let board = SudokuBoard(index: 0, withParent: nil)
     
     var boxes:[Box]
     var tiles: [Tile]
+    var boxRows = [[Box]]()
+    var boxColumns = [[Box]]()
+    var tileRows = [Tile]()
+    var tileColumns = [Tile]()
     
+
     required init(coder aDecoder: NSCoder) {
-        let someBoxes = board.convertBoxesToContainBoxes()
+        let someBoxes = board.boxes as! [Box]
         boxes = someBoxes
         tiles = {
             var mutableTiles = [Tile]()
             for box in someBoxes {
-                mutableTiles.extend(box.convertBoxesToContainTiles())
+                let containedTiles = box.boxes as! [Tile]
+                mutableTiles.extend(containedTiles)
             }
             return mutableTiles
             }()
         super.init(coder: aDecoder)
     }
     
+    func makeRowsAndColumns(){
+        for rc in 1...3 {
+            let aRow = board.makeRow(Row(rawValue: rc)!) as! [Box]
+            boxRows.append(aRow)
+            let aColumn = board.makeColumn(Column(rawValue: rc)!) as! [Box]
+            boxColumns.append(aColumn)
+        }
+    }
     func tileAtIndex(_index: TileIndex) -> Tile {
         return board.getBoxAtIndex(_index.0).getTileAtIndex(_index.1)
     }
-    
 }
--- SudokuObjects.swift
+++ SudokuObjects.swift
@@ -10,26 +10,68 @@ import UIKit
 
 class SudokuItem:UIView {
     var parentSquare: SudokuItem?
-    var index = 0
+    var defaultIndex = 0
+    
+    required init(index: Int, withParent: SudokuItem?) {
+        super.init(frame: CGRectZero)
+        self.defaultIndex = index
+        parentSquare = withParent
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
 }
 
+protocol Nestable {
+    var index:Int {get}
+    
+}
 
-class SudokuBoard: SudokuItem, SquareNester {
-    typealias NestingSquare = Box
-    
+enum Row: Int {
+    case One = 1
+    case Two = 2
+    case Three = 3
+}
+
+enum Column: Int {
+    case One = 1
+    case Two = 2
+    case Three = 3
+}
+
+protocol Nester {
+    var boxes: [SudokuItem] {get set}
+    func makeRow(row: Row) -> [SudokuItem]
+    func makeColumn(column: Column) -> [SudokuItem]
+    func view() -> UIView
+}
+
+extension SudokuItem: Nestable {
+    // MARK: Nester confromance
+    var index: Int {
+        get {
+            return self.defaultIndex
+        }
+    }
+}
+
+class SudokuBoard: SudokuItem, Nester {
+   
     var boxes: [SudokuItem]
-    var delegate: SquareNesterDelegate?
-    
-    override init(frame: CGRect) {
-        self.boxes = [Box]()
-        super.init(frame: frame)
+
+
+    required init(index: Int, withParent: SudokuItem?) {
+        boxes = [Box]()
+        super.init(index: index, withParent: withParent)
         
         for index in 0...8 {
-            let aBox = Box(index: index)
+            let aBox = Box(index: index, withParent:self)
             boxes.append(aBox)
-            self.addSubview(aBox)
+            self.addSubview(aBox.view())
+    
         }
-        
         self.prepareBoxes()
     }
     
@@ -38,8 +80,32 @@ class SudokuBoard: SudokuItem, SquareNester {
         fatalError("init(coder:) has not been implemented")
     }
     
+    func makeRow(row: Row)-> [SudokuItem] {
+        switch row {
+        case .One:
+            return [boxes[0], boxes[1], boxes[2]]
+        case .Two:
+            return [boxes[3], boxes[4], boxes[5]]
+        case .Three:
+            return [boxes[6], boxes[7], boxes[8]]
+        }
+    }
+    
+    func makeColumn(column: Column) -> [SudokuItem]{
+        switch column {
+        case .One:
+            return [boxes[0], boxes[3], boxes[6]]
+        case .Two:
+            return [boxes[1], boxes[4], boxes[7]]
+        case .Three:
+            return [boxes[2], boxes[5], boxes[8]]
+        }
+    }
+    
+    
     func prepareBoxes() {
-        for box in boxes {
+        for nested in boxes {
+            let box = nested as SudokuItem
             box.parentSquare = self
             box.backgroundColor = UIColor.lightGrayColor()
             box.layer.borderColor = UIColor.blackColor().CGColor
@@ -47,42 +113,28 @@ class SudokuBoard: SudokuItem, SquareNester {
             box.userInteractionEnabled = true
             box.setTranslatesAutoresizingMaskIntoConstraints(false)
         }
-        self.delegate = BoxSetter(squareNester: self)
-        self.delegate = BoxSetter(squareNester: self)
-        let constraints = self.delegate!.configureBoxConstraints()
+        
+        let constraints = BoxSetter().configureConstraintsForParentSquare(self)
         self.addConstraints(constraints)
     }
     
     
-    func sudokuItem() -> SudokuItem {
+    func view() -> UIView {
         return self
     }
     
-    func convertBoxesToContainBoxes() -> [Box] {
-        var boxesToReturn = [Box]()
-        for box in boxes {
-            let boxAs = box as! Box
-            boxesToReturn.append(boxAs)
-        }
-        return boxesToReturn
-    }
-    
-    func getBoxAtIndex(index: Int) -> Box {
-        return boxes[index] as! Box
-    }
 }
 
 
-class Box: SudokuItem, SquareNester, Nestable {
-    typealias NestingSquare = Tile
-    var boxes = [SudokuItem]()
-    var delegate: SquareNesterDelegate?
+class Box: SudokuItem, Nester{
     
-    init(index withIndex: Int){
-        super.init(frame: CGRectZero)
-        self.index = withIndex
+    var boxes: [SudokuItem]
+    
+    required init (index withIndex: Int, withParent: SudokuItem?){
+        boxes = [Tile]()
+        super.init(index: withIndex, withParent: withParent)
         for index in 0...8 {
-            let aBox = Tile(index: index, parent: self)
+            let aBox = Tile(index: index, withParent: self)
             boxes.append(aBox)
             self.addSubview(aBox)
         }
@@ -95,231 +147,186 @@ class Box: SudokuItem, SquareNester, Nestable {
         fatalError("init(coder:) has not been implemented")
     }
     
-    func convertBoxesToContainTiles() -> [Tile] {
-        var boxesToReturn = [Tile]()
-        for box in boxes {
-            let boxAs = box as! Tile
-            boxesToReturn.append(boxAs)
+    func makeRow(row: Row)-> [SudokuItem] {
+        switch row {
+        case .One:
+            return [boxes[0], boxes[1], boxes[2]]
+        case .Two:
+            return [boxes[3], boxes[4], boxes[5]]
+        case .Three:
+            return [boxes[6], boxes[7], boxes[8]]
         }
-        
-        return boxesToReturn
     }
     
-    func sudokuItem() -> SudokuItem {
-        return self
+
+    func makeColumn(column: Column) -> [SudokuItem]{
+        switch column {
+        case .One:
+            return [boxes[0], boxes[3], boxes[6]]
+        case .Two:
+            return [boxes[1], boxes[4], boxes[7]]
+        case .Three:
+            return [boxes[2], boxes[5], boxes[8]]
+        }
     }
+
     
     func prepareBoxes() {
-        for box in boxes {
+        for nested in boxes {
+            let box = nested as SudokuItem
             box.backgroundColor = UIColor.whiteColor()
             box.layer.borderColor = UIColor.lightGrayColor().CGColor
             box.layer.borderWidth = 1.0
             box.userInteractionEnabled = true
             box.setTranslatesAutoresizingMaskIntoConstraints(false)
         }
-        self.delegate = BoxSetter(squareNester: self)
-        let constraints = self.delegate!.configureBoxConstraints()
+        
+        let constraints:[NSLayoutConstraint] = BoxSetter().configureConstraintsForParentSquare(self)
         self.addConstraints(constraints)
     }
     
-    func getTileAtIndex(index: Int) -> Tile {
-        return boxes[index] as! Tile
+    func view() -> UIView {
+        return self
     }
-    
 }
 
 class Tile: SudokuItem, Nestable {
     
-    typealias TileIndex = (Box:Int, Tile:Int)
     
-    required init(index withIndex: Int, parent: SudokuItem) {
-        super.init(frame: CGRectMake(0, 0, 0, 0))
-        index = withIndex
-        parentSquare = parent
+    
+    required init (index: Int, withParent: SudokuItem?) {
+        super.init(index: index, withParent: withParent)
     }
     
     required init(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    func tileIndex() -> TileIndex {
-        return (parentSquare!.index, index)
+    
+    func view() -> UIView {
+        return self
     }
-    
-    func indexString() -> String {
-        let box = tileIndex().0
-        let tile = tileIndex().1
-        return "This tile's index is: \(box).\(tile) "
-    }
-    
-}
-
-protocol SquareNester  {
-    var boxes: [SudokuItem] {get set}
-    var delegate:SquareNesterDelegate? {get set}
-    func sudokuItem() -> SudokuItem
-    
-}
-
-protocol SquareNesterDelegate {
-    
-    var boxes: [SudokuItem] {get set}
-    var row1: [SudokuItem] { get set}
-    var row2: [SudokuItem] { get set}
-    var row3: [SudokuItem] { get set}
-    var column1: [SudokuItem] { get set}
-    var column2: [SudokuItem] { get set}
-    var column3: [SudokuItem] { get set}
-    
-    init (squareNester: SquareNester)
-    
-    func configureBoxConstraints() -> [NSLayoutConstraint]
-}
-
-protocol Nestable {
-    // there might be a use for this down the road...
 }
 
 
-class BoxSetter: SquareNesterDelegate {
+    class BoxSetter {
     
-    var clientItem: SudokuItem
-    var boxes:[SudokuItem]
-    var row1: [SudokuItem]
-    var row2: [SudokuItem]
-    var row3: [SudokuItem]
-    var column1: [SudokuItem]
-    var column2: [SudokuItem]
-    var column3: [SudokuItem]
-    
-    required init (squareNester: SquareNester) {
-        boxes = squareNester.boxes
-        row1 = [boxes[0], boxes[1], boxes[2]]
-        row2 = [boxes[3], boxes[4], boxes[5]]
-        row3 = [boxes[6], boxes[7], boxes[8]]
-        column1 = [boxes[0], boxes[3], boxes[6]]
-        column2 = [boxes[1], boxes[4], boxes[7]]
-        column3 = [boxes[2], boxes[5], boxes[8]]
-        
-        clientItem = squareNester.sudokuItem()
-    }
-    
-    func configureBoxConstraints() -> [NSLayoutConstraint] {
+    func configureConstraintsForParentSquare<U:UIView where U:Nester>(square: U) -> [NSLayoutConstraint] {
         var constraints = [NSLayoutConstraint]()
         
-        for box in row1 {
-            constraints.extend(self.makeLayoutConstraintsForBox(box, forAttributes: [.Top, .Width, .Height,]))
+        for box in square.makeRow(.One) {
+            constraints.extend(self.makeLayoutConstraintsForBox(box, inBox: square, forAttributes: [NSLayoutAttribute.Top, .Width, .Height]))
         }
         
-        for box in row3 {
-            constraints.extend(self.makeLayoutConstraintsForBox(box, forAttributes: [.Bottom, .Width, .Height]))
+        for box in square.makeRow(.Three) {
+            constraints.extend(self.makeLayoutConstraintsForBox(box, inBox: square, forAttributes: [.Bottom, .Width, .Height]))
         }
         
-        for box in column1 {
-            constraints.extend(self.makeLayoutConstraintsForBox(box, forAttributes: [.Leading, .Width, .Height]))
+        for box in square.makeColumn(.One) {
+            constraints.extend(self.makeLayoutConstraintsForBox(box, inBox: square, forAttributes: [.Leading, .Width, .Height]))
         }
         
-        for box in column3 {
-            constraints.extend(self.makeLayoutConstraintsForBox(box, forAttributes: [.Trailing, .Width, .Height]))
+        for box in square.makeColumn(.Three) {
+            constraints.extend(self.makeLayoutConstraintsForBox(box, inBox: square, forAttributes: [.Trailing, .Width, .Height]))
         }
         
-        for box in row2 {
-            constraints.extend(self.makeLayoutConstraintsForBox(box, forAttributes: [.Top, .Bottom, .Width, .Height]))
+        for box in square.makeRow(.Two) {
+            constraints.extend(self.makeLayoutConstraintsForBox(box, inBox: square, forAttributes: [.Top, .Bottom, .Width, .Height]))
         }
         
-        for box in column2 {
-            constraints.extend(self.makeLayoutConstraintsForBox(box, forAttributes: [.Leading, .Trailing]))
+        for box in square.makeColumn(.One) {
+            constraints.extend(self.makeLayoutConstraintsForBox(box, inBox: square, forAttributes: [.Leading, .Trailing]))
             
         }
         
         return constraints
     }
+
     
     
-    func makeLayoutConstraintsForBox(aBox: SudokuItem, forAttributes attributes:[NSLayoutAttribute]) -> [NSLayoutConstraint] {
+    func makeLayoutConstraintsForBox<T:UIView, U:UIView where T: Nestable, U:Nester>(box: T, inBox parentBox: U, forAttributes attributes:[NSLayoutAttribute]) -> [NSLayoutConstraint] {
         var constraints = [NSLayoutConstraint]()
         
         for attribute in attributes {
-            constraints.append(self.makeLayoutConstraintForBox(aBox, forAttribute: attribute))
+            constraints.append(self.makeLayoutConstraintForBox(box, inBox: parentBox, forAttribute: attribute))
         }
         
         return constraints
     }
     
-    private func makeLayoutConstraintForBox(aBox: SudokuItem, forAttribute anAttribute: NSLayoutAttribute) -> NSLayoutConstraint {
+    func makeLayoutConstraintForBox<T:UIView, U:UIView where T:Nestable, U:Nester>(box: T, inBox parentBox: U, forAttribute anAttribute: NSLayoutAttribute) -> NSLayoutConstraint {
         switch anAttribute{
         case .Width, .Height:
-            return NSLayoutConstraint(item: aBox, attribute: anAttribute, relatedBy: NSLayoutRelation.Equal, toItem: aBox.parentSquare, attribute: anAttribute, multiplier: 1/3, constant: 0)
+            return NSLayoutConstraint(item: box, attribute: anAttribute, relatedBy: NSLayoutRelation.Equal, toItem: parentBox.view(), attribute: anAttribute, multiplier: 1/3, constant: 0)
         default:
-            let neighborItem = self.getNeighborForBoxIndex(aBox.index, forAttribute: anAttribute)!
+            let neighborItem = self.getNeighborForBoxIndex(box.index, inParent: parentBox, forAttribute: anAttribute)!
             let neighborAttribute = neighborItem.attribute
             let neighborView = neighborItem.neighbor
-            return NSLayoutConstraint(item: aBox, attribute: anAttribute, relatedBy: NSLayoutRelation.Equal, toItem: neighborView, attribute: neighborAttribute, multiplier: 1, constant: 0)
+            return NSLayoutConstraint(item: box, attribute: anAttribute, relatedBy: NSLayoutRelation.Equal, toItem: neighborView, attribute: neighborAttribute, multiplier: 1, constant: 0)
             
         }
         
     }
     
     // MARK: Constraint maker helpers
-    typealias ToItem = (neighbor:SudokuItem, attribute:NSLayoutAttribute)?
+    typealias ToItem = (neighbor:UIView, attribute:NSLayoutAttribute)?
     
-    func getNeighborForBoxIndex(index: Int, forAttribute anAttribute:NSLayoutAttribute) -> ToItem {
+    func getNeighborForBoxIndex<U:UIView where U:Nester>(index: Int, inParent parent: U, forAttribute anAttribute:NSLayoutAttribute) -> ToItem {
         switch anAttribute {
         case .Bottom:
-            return self.getBottomNeighborForBoxIndex(index)
+            return self.getBottomNeighborForBoxIndex(index, inParent: parent)
         case .Top:
-            return self.getTopNeighborForBoxIndex(index)
+            return self.getTopNeighborForBoxIndex(index, inParent: parent)
         case .Left,.Leading:
-            return self.getLeftNeighborForBoxIndex(index)
+            return self.getLeftNeighborForBoxIndex(index, inParent: parent)
         case .Right,.Trailing:
-            return self.getRightNeighborForBoxIndex(index)
+            return self.getRightNeighborForBoxIndex(index, inParent: parent)
         default:
             return nil
         }
     }
     
-    func getTopNeighborForBoxIndex(index: Int) -> ToItem {
+    func getTopNeighborForBoxIndex<U:UIView where U:Nester>(index: Int, inParent parent: U) -> ToItem {
         switch index {
         case 3...8:
-            return (boxes[index-3], .Bottom)
+            return (parent.boxes[index-3], .Bottom)
         case 0...2:
-            return (clientItem, .Top)
+            return (parent.view(), .Top)
         default:
             return nil
         }
     }
     
-    func getBottomNeighborForBoxIndex(index: Int) -> ToItem {
+    func getBottomNeighborForBoxIndex<U:UIView where U:Nester>(index: Int, inParent parent: U) -> ToItem {
         switch index {
         case 0...5:
-            return (boxes[index+3], .Top)
+            return (parent.boxes[index+3], .Top)
         case 5...8:
-            return (clientItem, .Bottom)
+            return (parent.view(), .Bottom)
         default:
             return nil
         }
     }
     
-    func getLeftNeighborForBoxIndex(index: Int) ->ToItem {
+    func getLeftNeighborForBoxIndex<U:UIView where U:Nester>(index: Int, inParent parent: U) -> ToItem {
         switch index {
         case 0,3,6:
-            return (clientItem, .Leading)
+            return (parent.view(), .Leading)
         case 1,2,4,5,7,8:
-            return (boxes[index-1], .Trailing)
+            return (parent.boxes[index-1], .Trailing)
         default:
             return nil
         }
     }
     
-    func getRightNeighborForBoxIndex(index: Int) -> ToItem {
+    func getRightNeighborForBoxIndex<U:UIView where U:Nester>(index: Int, inParent parent: U) -> ToItem {
         switch index {
         case 2,5,8:
-            return (clientItem, .Trailing)
+            return (parent.view(), .Trailing)
         case 0,1,3,4,6,7:
-            return (boxes[index+1], .Leading)
+            return (parent.boxes[index+1], .Leading)
         default:
             return nil
         }
     }
-    
 }
